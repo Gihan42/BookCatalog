28f938be3fd436676f2b8f6d22c181010ade802e
Gihan42 <gihanm729@gmail.com>
2023-06-24 20:56:39 +0530

    updated

--- src/Pages/Crud.js
+++ src/Pages/Crud.js
@@ -18,7 +18,47 @@ export default class Crud extends Component {
         <div className="cursor-pointer w-full pt-2 border-2 border-white pb-2 pl-6 bg-cyan-950 text-white rounded flex justify-between items-center ">
              <h1 className='text-2xl '>Books Details</h1>
           </div>
-        
+          {/* conten */}
+          
+          <div className='grid grid-cols-2 gap-4'>
+            <div>01
+              
+              
+                </div>
+                  
+            <div className='p-10'>
+              
+              <table class="table text-2xl">
+                      <thead>
+                        <tr>
+                          <th scope="col">Book Id</th>
+                          <th scope="col">Book Name</th>
+                          <th scope="col">Description</th>
+                          <th scope="col">Aouther</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>Mark</td>
+                          <td>Otto</td>
+                          <td>@mdo</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">2</th>
+                          <td>Jacob</td>
+                          <td>Thornton</td>
+                          <td>@fat</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">3</th>
+                          <td colspan="2">Larry the Bird</td>
+                          <td>@twitter</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                 </div>
+          </div>
       </div>
     
       </>
