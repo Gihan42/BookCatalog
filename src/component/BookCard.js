6d9ae2a4ca39abd5d2e05cf0518ece48aa1a52a1
Gihan42 <gihanm729@gmail.com>
2023-06-24 19:40:50 +0530

    update

--- src/component/BookCard.js
+++ src/component/BookCard.js
@@ -5,20 +5,21 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import img from '../assets/book-composition-with-open-book.jpg'
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
+import photo from '../assets/book-composition-with-open-book.jpg'
 export default class BookCard extends Component {
     render() {
     return (
-        <div >
-              <Card className='hover:bg-slate-300 drop-shadow-2xl shadow-black rounded-3xl border cursor-pointer ' sx={{ maxWidth: 345 }}>
+        <div>
+      <Card className='hover:bg-slate-300 drop-shadow-2xl shadow-black rounded-3xl border cursor-pointer ' sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
         alt="green iguana"
         height="140"
-        image="../assets/book-composition-with-open-book.jpg"
-      />
+        image="../assets/book-composition-with-open-book.jpg"   
+          />
+          <photo/>
       <CardContent>
         <Typography gutterBottom variant="h4" component="div">
          Book Name
@@ -27,8 +28,8 @@ export default class BookCard extends Component {
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
             </Typography>
-             <Typography gutterBottom variant="h5" component="div">
-              Author : 
+             <Typography gutterBottom variant="h5" component="div" className='flex ml-3'>
+              Author : <div> Martin</div>
         </Typography>
       </CardContent>
       <CardActions>
