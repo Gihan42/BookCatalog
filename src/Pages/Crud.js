489efac19aa3bd5d8a32aac02bd00f645cdfc755
Gihan42 <gihanm729@gmail.com>
2023-06-24 21:39:09 +0530

    updated

--- src/Pages/Crud.js
+++ src/Pages/Crud.js
@@ -5,13 +5,16 @@ import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import bookphot from'../assets/book.png'
 import { TextField } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import UpdateIcon from '@mui/icons-material/Update';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default class Crud extends Component {
   render() {
     return (
       <>
         <Header/>
-        <div className='w-full mt-28 space-y-6 p-11 justify-center'>
+        <div className='bg-slate-100 w-full mt-20 space-y-6 p-11 justify-center'>
           <div className='font-Poppins font-bold text-cyan-900 text-4xl flex	justify-center'>
             <img src={bookphot} alt=" " className='h-16 w-16 mr-3' />
            <h1 className='mt-1'>Book Catalog </h1> 
@@ -23,7 +26,7 @@ export default class Crud extends Component {
           {/* conten */}
            <from>
           <div className='grid grid-cols-2 gap-4 mt-9'>
-            <div className='p-5 ml-2 border-1 rounded-2xl shadow-2xl shadow-black'>01
+            <div className=' bg-white p-5 ml-2 border-1 rounded-2xl shadow-2xl shadow-black'>
                
                 <div className='flex justify-center space-x-4'>
                             <TextField
@@ -37,7 +40,79 @@ export default class Crud extends Component {
                   />
                   <button type="button" class="btn btn-success">Search <SearchIcon/></button>
                 </div>
+            <div className='flex justify-start mt-6 space-x-20 '>
+                <div className='text-xl'>Book Id
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="DId"
+                  label="Book Id"
+                  name="DId"
+                  autoFocus
+                  type="text"
+                  variant="standard"
 
+                  value={""}
+                  onChange={""}
+                />
+                  </div>
+                  <div className='text-xl'>Book Name
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="DId"
+                  label="Book Name"
+                  name="DId"
+                  autoFocus
+                  type="text"
+                  variant="standard"
+
+                  value={""}
+                  onChange={""}
+                />
+                  </div>
+              </div>
+            <div className='flex justify-start mt-6 space-x-20 '>
+                <div className='text-xl space-y-2'>Description
+                <TextField
+                    label="Description"
+                    type="text"
+                    variant='outlined'
+                    placeholder='Type Description Here'
+                    sx={{ width: 800 }}
+                    multiline
+                    minRows={4}
+                    maxRows={Infinity}
+                    required
+
+                  value={""}
+                  onChange={""}
+                />
+                 </div>
+                </div>
+                <div className='flex justify-start mt-3 space-x-6 '>
+                <div className='text-xl'>Author Name
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="DId"
+                  label="Author Name"
+                  name="DId"
+                  autoFocus
+                  type="text"
+                  variant="standard"
+                  value={""}
+                  onChange={""}
+                    />
+               
+                  </div>
+                <button type="button" class="btn btn-primary  mt-5 h-12 w-28">Save <SaveAltIcon/></button>
+                <button type="button" class="btn btn-warning  mt-5 h-12 w-28">Update<UpdateIcon/> </button>
+                <button type="button" class="btn btn-danger  mt-5 h-12 w-28">Delete <DeleteIcon/></button>
+                  </div>
             </div>
                   
             <div className='p-10'>
